69d72de30cb5bbbeb1d894445916be287857e32a
cuppachino <cuppachino.dev@gmail.com>
2022-11-27 11:50:02 -0600

    Add `Simplify` type

---
+++ src/simplify.ts
@@ -0,0 +1,5 @@
+/**
+ * Create a new, mapped type from `T`.
+ * @example Simplify<{ a: 1 } & { b: 2 }> // { a: 1, b: 2 }
+ */
+export type Simplify<T> = T extends infer U ? { [K in keyof U]: U[K] } : never;
