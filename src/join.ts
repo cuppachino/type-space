e5bf77b2983b7fa908df53d02e4c64ec2ba7409b
cuppachino <cuppachino.dev@gmail.com>
2022-12-01 19:18:53 -0600

    :bug: Fix trailing delimiter in `Join`

--- src/join.ts
+++ src/join.ts
@@ -2,6 +2,6 @@ export type Join<
   T extends string[],
   D extends string = '',
   Acc extends string = ''
-> = T extends [infer Head extends string, ...infer Tail extends any[]]
-  ? Join<Tail, D, `${Acc}${Head}${D}`>
+> = T extends [infer Head extends string, ...infer Tail extends string[]]
+  ? Join<Tail, D, `${Acc}${Head}${Tail extends [] ? '' : D}`>
   : Acc
