e996ebe25badf43c40be90c09fa2093c3a8c31a9
cuppachino <cuppachino.dev@gmail.com>
2022-12-02 01:04:44 -0600

    :heavy_plus_sign: Add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+/** @type { import("@types/eslint").Linter.BaseConfig } */
+module.exports = {
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		/* 🎨 Prettier - Needs to be last so it can override other configs */
+		'plugin:prettier/recommended'
+	],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		tsconfigRootDir: __dirname,
+		project: ['./tsconfig.json', './tsconfig.eslint.json']
+	},
+	plugins: ['@typescript-eslint', 'prettier'],
+	rules: {
+		// * 🔗 Imports
+		'@typescript-eslint/consistent-type-imports': [
+			'error',
+			{
+				prefer: 'type-imports',
+				disallowTypeAnnotations: true
+			}
+		],
+		'sort-imports': [
+			'error',
+			{
+				ignoreCase: false
+			}
+		]
+	}
+}
