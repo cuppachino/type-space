980186380106673bebca2906d72f0ed6ee90c756
cuppachino <cuppachino.dev@gmail.com>
2022-12-14 17:26:38 -0600

    :rotating_light: Test `CreateTuple`

---
+++ tests/create-tuple.test.ts
@@ -0,0 +1,8 @@
+import type { CreateTuple } from '../src'
+import { expectType } from 'tsd-lite'
+
+declare const case1: CreateTuple<4>
+expectType<[never, never, never, never]>(case1)
+
+declare const case2: CreateTuple<3, 'a'>
+expectType<['a', 'a', 'a']>(case2)
