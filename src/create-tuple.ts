c53139366745e71211a3700e2a8a8b67c9752ae3
cuppachino <cuppachino.dev@gmail.com>
2022-12-14 16:18:28 -0600

    :sparkles: Add `CreateTuple` type

---
+++ src/create-tuple.ts
@@ -0,0 +1,12 @@
+import { Length } from 'type-space'
+
+/**
+ * Generates a fixed-length N tuple filled with type T.
+ * @example Fill<5> // [never, never, never, never, never]
+ * @example Fill<3, 'a'> // ['a', 'a', 'a']
+ */
+export type CreateTuple<
+	L extends number,
+	T = never,
+	A extends T[] = []
+> = Length<A> extends L ? A : CreateTuple<L, T, [...A, T]>
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export type { Absolute } from 'type-space/absolute'
+export type { CreateTuple } from 'type-space/create-tuple'
 export type { IndexOf } from 'type-space/index-of'
 export type { Indices } from 'type-space/indices'
 export type { Join } from 'type-space/join'
