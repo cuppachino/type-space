f30314a03a681d8924f2e7c41f0ba353cc621d6b
cuppachino <cuppachino.dev@gmail.com>
2022-12-14 18:48:44 -0600

    📚 Document `Join` type

--- src/join.ts
+++ src/join.ts
@@ -1,3 +1,9 @@
+/**
+ Joins an array of strings into a single string, separated by a delimiter.
+ 
+ @example Join<['apple', 'banana', 'plum'], ' & '> // 'apple & banana & plum'
+ @example Join<['a', 'b', 'c']> // 'abc'
+ */
 export type Join<
 	T extends string[],
 	D extends string = '',
