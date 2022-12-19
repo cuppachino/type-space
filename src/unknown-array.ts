0081b979c901dc99ae3a51e3d88581fd2a9b895d
cuppachino <cuppachino.dev@gmail.com>
2022-12-19 03:43:23 -0600

    :bulb: Document `UnknownArray` type

--- src/unknown-array.ts
+++ src/unknown-array.ts
@@ -1,1 +1,6 @@
+/**
+ Represents an array or tuple of unknown elements.
+ 
+ @example declare function foo<T extends UnknownArray>(...args: T): Foo<T>
+ */
 export type UnknownArray = unknown[] | readonly unknown[]
