6f67b36fedc0cde0e543a86dd42425882413542a
Jacob Bergholtz <cuppachino.dev@gmail.com>
2022-12-19 16:32:23 -0600

    ✍️ Edit source docs

--- src/extends/extends-function.ts
+++ src/extends/extends-function.ts
@@ -4,8 +4,7 @@
  @param T - The type to check.
  @param R - The type to resolve to if `T` extends a function `(default: T)`.
  @see
- {@link [source tests ](https://github.com/Cuppachino/type-space/blob/main/tests/extends-extract-functions.test.ts)}
- for a fun example of how to use this type.
+ {@link [ExtractFunctions ](https://github.com/Cuppachino/type-space/blob/main/src/extract/extract-functions.ts)} for example usage.
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type ExtendsFunction<T, R = T> = T extends (...args: any[]) => any
