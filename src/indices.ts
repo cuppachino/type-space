1203d1bddd82bd14c40c94de2d9ca38e6d50aa7d
cuppachino <cuppachino.dev@gmail.com>
2023-01-04 09:06:06 -0600

    ✍️ Fix `IndicesOf` doc comment link

--- src/indices.ts
+++ src/indices.ts
@@ -1,3 +1,6 @@
+import type { IndexOf } from './index-of'
+import type { NumberLiteral } from './number-literal'
+import type { ParseNumberLiteral } from './parse-number-literal'
 /**
  * Generates a tuple based on the length of the input array and fills each index with the index number.
  *
@@ -11,7 +14,9 @@
  * type FruitIndex = Indices<['apple', 'banana', 'plum']>[number] // 0 | 1 | 2
  * ```
  * @see {@link IndexOf}
+ * @see {@link NumberLiteral}
+ * @see {@link ParseNumberLiteral}
  */
-export type Indices<T extends unknown[] | readonly unknown[]> = {
-	[K in keyof T]: K extends `${infer N extends number}` ? N : never
+export type Indices<T> = {
+	[K in keyof T]: K extends NumberLiteral ? ParseNumberLiteral<K> : never
 }
