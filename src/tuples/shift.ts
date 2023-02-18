778e1b4e73ac9ee8ebe161da32d2c456c6e97b20
cuppachino <cuppachino.dev@gmail.com>
2023-02-17 20:31:48 -0600

    ✨ Add `push`, `pop`, `shift`, `unshift`

---
+++ src/tuples/shift.ts
@@ -0,0 +1,14 @@
+import type { UnknownArray } from '../unknown-array'
+
+/**
+ * Remove the first element from a tuple type. Does not return the removed type.
+ *
+ * @example
+ * ```
+ * Shift<[1, 2, 3]> // [2, 3]
+ * ```
+ * @remarks Its worth noting `Pop`, `Push`, `Shift`, and `Unshift` are inspired by JS; however, the types are not 1:1 for design reasons. More information can be found in [README](https://github.com/Cuppachino/type-space/#type-space).
+ */
+export type Shift<T extends UnknownArray> = T extends [any, ...infer U]
+	? U
+	: never
